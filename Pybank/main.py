c68dc27c3b1d0899c31d8b6ee681787d6a391ab7
M. Forrest Badgley <forrestbadgley@Ms-MacBook-Pro.local>
2018-02-16 23:02:00 -0600

    Finished

--- Pybank/main.py
+++ Pybank/main.py
@@ -15,8 +15,7 @@ Revenue_Change = []
 Total_Months2 = []
 Total_Revenue2 = 0
 Revenue_Change2 = []
-Date_format = []
-Date_format2 = []
+
 
 #doing everything 2x to open and load data for the two files
 # Open the CSV; the encoding line was added after getting error in reading the file and searching google for how to solve
@@ -43,8 +42,7 @@ with open(csvpath, newline="", encoding = "ISO-8859-1") as csvfile, open(csvpath
         #put the revenue numbers into a list
         Revenue_Change2.append(int(row[1]))
     
-    #absolute value of the incremental change between revenue nums
-    v = [abs((Revenue_Change[i+1]-Revenue_Change[i])) for i in range(len(Revenue_Change)-1)]
+   
     #create a list of the revenue changes in csv file 1, called c
     c = [((Revenue_Change[i+1]-Revenue_Change[i])) for i in range(len(Revenue_Change)-1)]
     #create a list of the revenue changes in csv file2, called d
@@ -60,37 +58,23 @@ with open(csvpath, newline="", encoding = "ISO-8859-1") as csvfile, open(csvpath
     Tot_months = (datetime.datetime.strptime(i, "%b-%y") for i in Tot_months)
     Tot_months2 = (datetime.datetime.strptime(i,"%b-%Y") for i in Tot_months2)
 
-    #for date in Tot_months:
-       # Date_format.append(datetime.datetime.strftime(date, "%Y-%m-%d"))
-       #Tot_months3 = (datetime.datetime.strptime(i, "%Y-%m-%d") for i in Tot_months)
-
-    #create a dictionary of the months, lined up with the revenue changes
-    
-    #d1 = dict(zip(Tot_months,c))
-    #d2 = dict(zip(Tot_months2, d))
-    
+   #create a dataframe from lists Tot months and c(revenue changes); name columns Date and Revenue
     d2_df = pd.DataFrame(list(zip(Tot_months, c)))
     d2_df.columns=["Date","Revenue"]
-   # d2_df['Date'] = pd.to_datetime(d2_df['Date'])
-    #d2_df['Date'] = d2_df['Date'].dt.strftime('%m/%Y')
-    #d2_df.Date = pd.to_datetime(d2_df.Date)
-    #df2.month = pd.to_datetime(df2.month)
-    
+  
+    #create a dataframe from lists Tot months2 and d(revenue changes); name columns Date and Revenue
     d1_df = pd.DataFrame(list(zip(Tot_months2, d)))
     d1_df.columns=["Date","Revenue"]
-    #d1_df['Date'] = pd.to_datetime(d1_df['Date'])
-    #d1_df['Date'] = d1_df['Date'].dt.strftime('%m/%Y')
     
-    #left outer join of dataframes
+    
+    #left outer join of dataframes on the Date column, budget_data_1(d2_df) is subset of daterange of
+    #budget_data_2(d1_df), all d1_df will show up and d2_df will align on shared dates; 
     d3_df = pd.merge(d1_df, d2_df, on='Date', how='left')
+    #replace NaNs with 0, where d2_df did not have dates or revenues, but d1_df did; 
     d3_df.fillna(0, inplace=True)
     
     #create new column, sum of the two revenue change columns, 
     d3_df["Sum"] = d3_df["Revenue_x"]+d3_df["Revenue_y"]
-  
-    #d2_df = pd.DataFrame([d2])
-    #d1_df = pd.DataFrame(d1, columns=['Date', 'Revenue'], index=0)  
-    #d1_df = d1_df.set_index('Date')  
 
    #find the min and max revenue change #'s
     max_increase_revenue = d3_df['Sum'].max()
@@ -113,18 +97,15 @@ print("Average Revenue Change: " + "$"+"{:,}".format(Average_Revenue_Change))
 print("Maximum Revenue Increase: " + (date_max.to_string(index=False)) + " " + "$"+"{:,}".format(int(max_increase_revenue)))
 print("Maximum Revenue Decrease: " + (date_min.to_string(index=False)) + " " + "$"+"{:,}".format(int(max_decrease_revenue)))
 
-output = ['Financial Analysis: ' , 'Total Months: ' + (str(months_count)), "Total Revenue: " + "$"+"{:,}".format((Total_Revenue+Total_Revenue2)), "Average Revenue Change: " + "$"+"{:,}".format(Average_Revenue_Change), "Maximum Revenue Increase: " + (date_max.to_string(index=False)) + " " + "$"+"{:,}".format(int(max_increase_revenue)), "Maximum Revenue Decrease: " + (date_min.to_string(index=False)) + " " + "$"+"{:,}".format(int(max_decrease_revenue))]
 
+#store in a variable
+output = ['Financial Analysis: ' , 'Total Months: ' + (str(months_count)), "Total Revenue: " + "$"+"{:,}".format((Total_Revenue+Total_Revenue2)), "Average Revenue Change: " + "$"+"{:,}".format(Average_Revenue_Change), "Maximum Revenue Increase: " + (date_max.to_string(index=False)) + " " + "$"+"{:,}".format(int(max_increase_revenue)), "Maximum Revenue Decrease: " + (date_min.to_string(index=False)) + " " + "$"+"{:,}".format(int(max_decrease_revenue))]
+#write to textfile
 output_path = ('/Users/forrestbadgley/Documents/DataScience/python-challenge/Pybank/outputfile')
 with open(output_path, 'w', newline='') as text_file:
         text_file.writelines('\n'.join(output))
 
 
-
-#import sys
-#filename = open("/Users/forrestbadgley/Documents/DataScience/python-challenge/Pybank/outputfile",'w')
-#3sys.stdout 
-#text_file.writelines(output)
 
 
    
